5cad0f683cbf1971b7b835c601f550e09be98e20
Maxim Kucheriaviy <maxim-k@i.ua>
2022-12-31 16:24:22 +0200

    table fix

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -5,15 +5,17 @@ import PropTypes from "prop-types";
 export const ContactsList = ({contacts, deleteContact}) => {
   return(
     <ContactList>
-      {contacts.map(item => {
-        return(
-          <tr>
-            <td>{item.name}:</td> 
-            <td>{item.number}</td> 
-            <td><button onClick={() => {deleteContact(item.id)}}>delete</button></td>
-          </tr>
-        )
-      })}
+        <tbody>
+          {contacts.map(item => {
+            return(
+              <tr key={item.id}>
+                <td>{item.name}:</td> 
+                <td>{item.number}</td> 
+                <td><button onClick={() => {deleteContact(item.id)}}>delete</button></td>
+              </tr>
+            )
+          })}
+        </tbody>
     </ContactList>
   )
 }
